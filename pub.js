344a9063a2f293b2bce78fb8efbddef28d2333d8
Jeudi Prando <jeudiprando@gmail.com>
2020-02-19 21:37:54 -0300

    ajustes finos

--- pub.js
+++ pub.js
@@ -3,6 +3,6 @@ var redis = require('redis')
 var publisher = redis.createClient({ host: 'redis-db' })
 
 setInterval(() => {
-  console.log('pub msg...')
+  console.log(`[ ${new Date().toISOString()} ] pub msg...`)
   publisher.publish('notification', '{”message”:”Hello world from Asgardian!”}')
 }, 1000)
--- sub.js
+++ sub.js
@@ -3,7 +3,7 @@ var redis = require('redis')
 var subscriber = redis.createClient({ host: 'redis-db' })
 
 subscriber.on('message', function (channel, message) {
-  console.log('Message: ' + message + ' on channel: ' + channel + ' is arrive!')
+  console.log(`[ ${new Date().toISOString()} ] Message: ` + message + ' on channel: ' + channel + ' is arrive!')
 })
 
 subscriber.subscribe('notification')
